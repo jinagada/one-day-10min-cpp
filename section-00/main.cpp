4278ad130376f93f9fde91c73c5b872c43f132d4
jinagada <kimyg3174@gmail.com>
2023-11-07 14:47:32 +0900

    최초등록

---
+++ section-00/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Frodo on 2023/11/07.
+//
+#include <iostream>
+
+//using namespace std;
+
+void hello_world() {
+    std::cout << "Hello, ";
+    std::cout << "World!" << std::endl;
+}
+
+int main() {
+    hello_world();
+    return 0;
+}
---
+++ section-01/main.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Frodo on 2023/11/07.
+//
+#include <iostream>
+
+using namespace std;
+
+void chapter_03() {
+    int a;
+    a = 7;
+    int b = 3;
+    a = 5;
+    b = 10;
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "a = " << &a << ", b = " << &b << endl;
+}
+
+void chaper_04() {
+
+}
+
+int main() {
+    chapter_03();
+    return 0;
+}
